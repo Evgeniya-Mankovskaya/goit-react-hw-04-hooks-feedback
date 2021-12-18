4cdc98715941b27673c8232c19105c50f78ec312
Yevheniia Mankivska <evgeniyamankovskaya@gmail.com>
2021-12-18 12:46:41 +0200

    deleted the commented code

--- src/App.js
+++ src/App.js
@@ -44,48 +44,4 @@ const App = () => {
     </div>
   );
 };
-// class App extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-//   stateFeedback = (e) => {
-//     this.setState((prevState) => ({
-//       [e]: prevState[e] + 1,
-//     }));
-//   };
-
-//   countTotalFeedback = function () {
-//     return this.state.good + this.state.neutral + this.state.bad;
-//   };
-//   countPositiveFeedbackPercentage = function () {
-//     return Math.floor((this.state.good * 100) / this.countTotalFeedback());
-//   };
-
-//   render() {
-//     let total = this.countTotalFeedback();
-//     return (
-//       <>
-//         <Section title="Please leave feedback" />
-//         <FeedbackOptions
-//           options={["good", "neutral", "bad"]}
-//           onFeedback={this.stateFeedback}
-//         />
-//         <Section title="Statistics" />
-//         {total === 0 ? (
-//           <Notification message="No feedback given" />
-//         ) : (
-//           <Statistics
-//             good={this.state.good}
-//             neutral={this.state.neutral}
-//             bad={this.state.bad}
-//             total={this.countTotalFeedback()}
-//             positiveFeedback={this.countPositiveFeedbackPercentage()}
-//           />
-//         )}
-//       </>
-//     );
-//   }
-// }
 export default App;
